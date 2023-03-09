6e3e066e7179c3a7d554e538dfb3180d21c70b81
Federico Berto <berto.federico2@gmail.com>
2023-03-10 04:23:37 +0900

    Add TSP TorchRL environment

---
+++ ncobench/envs/__init__.py
@@ -0,0 +1,1 @@
+from ncobench.envs.tsp import TSPEnv
---
+++ ncobench/envs/utils.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+from typing import Optional
+
+import numpy as np
+import torch
+import tqdm
+from tensordict.nn import TensorDictModule
+from tensordict.tensordict import TensorDict, TensorDictBase
+from torch import nn
+
+from torchrl.data import BoundedTensorSpec, CompositeSpec, UnboundedContinuousTensorSpec, BinaryDiscreteTensorSpec, UnboundedDiscreteTensorSpec
+from torchrl.envs import (
+    CatTensors,
+    EnvBase,
+    Transform,
+    TransformedEnv,
+    UnsqueezeTransform,
+)
+from torchrl.envs.transforms.transforms import _apply_to_composite
+from torchrl.envs.utils import check_env_specs, step_mdp
+
+
+
+def make_composite_from_td(td):
+    # custom funtion to convert a tensordict in a similar spec structure
+    # of unbounded values.
+    composite = CompositeSpec(
+        {
+            key: make_composite_from_td(tensor)
+            if isinstance(tensor, TensorDictBase)
+            else UnboundedContinuousTensorSpec(
+                dtype=tensor.dtype, shape=tensor.shape
+            )
+            for key, tensor in td.items()
+        },
+        shape=td.shape,
+    )
+    return composite
+
+
+def batch_to_scalar(param):
+    """Return first element if in batch. Used for batched parameters that are the same for all elements in the batch."""
+    if len(param.shape) > 0:
+        return param[0].item()
+    if isinstance(param, torch.Tensor):
+        return param.item()
+    return param
+
+
+def _set_seed(self, seed: Optional[int]):
+    rng = torch.manual_seed(seed)
+    self.rng = rng
