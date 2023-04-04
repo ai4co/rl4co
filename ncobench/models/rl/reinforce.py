1e27cebbebdb9e47574a3142aa6f2a72ccf6fb0f
Federico Berto <berto.federico2@gmail.com>
2023-04-05 03:53:40 +0900

    Move REINFORCE baselines

--- ncobench/models/rl/reinforce.py
+++ ncobench/models/rl/reinforce.py
@@ -4,6 +4,10 @@ from scipy.stats import ttest_rel
 import torch
 import torch.nn as nn
 
+from ncobench import utils
+
+log = utils.get_pylogger(__name__)
+
 
 class REINFORCEBaseline(nn.Module):
     def __init__(self, *args, **kwargs):
@@ -58,8 +62,8 @@ class WarmupBaseline(REINFORCEBaseline):
         self.alpha = 0
         self.n_epochs = n_epochs
 
-    def setup(self, model, dl, env=None):
-        self.baseline.setup(model, dl, env)
+    def setup(self, *args, **kwargs):
+        self.baseline.setup(*args, **kwargs)
 
     def eval(self, td, cost):
         if self.alpha == 1:
@@ -73,12 +77,12 @@ class WarmupBaseline(REINFORCEBaseline):
             1 - self.alpha * lw
         )
 
-    def epoch_callback(self, model, dl, epoch):
+    def epoch_callback(self, model, dl, epoch=None, env=None, device="cpu"):
         # Need to call epoch callback of inner model (also after first epoch if we have not used it)
-        self.baseline.epoch_callback(model, dl, epoch)
+        self.baseline.epoch_callback(model, dl, epoch, env, device)
         self.alpha = (epoch + 1) / float(self.n_epochs)
         if epoch < self.n_epochs:
-            print("Set warmup alpha = {}".format(self.alpha))
+            log.info("Set warmup alpha = {}".format(self.alpha))
 
 
 class RolloutBaseline(REINFORCEBaseline):
@@ -86,14 +90,13 @@ class RolloutBaseline(REINFORCEBaseline):
         super(RolloutBaseline, self).__init__()
         self.bl_alpha = bl_alpha
 
-    def setup(self, model, dl, env=None):
-        self._update_model(model, dl, env)
+    def setup(self, *args, **kwargs):
+        self._update_model(*args, **kwargs)
 
-    def _update_model(self, model, dl, env=None):
-        device = next(model.parameters()).device
+    def _update_model(self, model, dl, env=None, device="cpu"):
         self.model = copy.deepcopy(model).to(device)
-        print("Evaluating baseline model on evaluation dataset")
-        self.bl_vals = -self.rollout(model, dl, env).cpu().numpy()
+        log.info("Evaluating baseline model on evaluation dataset")
+        self.bl_vals = -self.rollout(model, dl, env, device).cpu().numpy()
         self.mean = self.bl_vals.mean()
 
     def eval(self, td, cost):
@@ -102,38 +105,35 @@ class RolloutBaseline(REINFORCEBaseline):
             out = -self.model(td)["reward"]
         return out, 0
 
-    def epoch_callback(self, model, dl, epoch=None, env=None):
+    def epoch_callback(self, model, dl, epoch=None, env=None, device="cpu"):
         """
         Challenges the current baseline with the model and replaces the baseline model if it is improved.
         """
-        print("\nEvaluating candidate model on evaluation dataset")
-        candidate_vals = -self.rollout(model, dl, env).cpu().numpy()
+        log.info("\nEvaluating candidate model on evaluation dataset")
+        candidate_vals = -self.rollout(model, dl, env, device).cpu().numpy()
         candidate_mean = candidate_vals.mean()
 
-        print("Candidate mean: {}".format(candidate_mean))
-        print("Baseline mean: {}".format(self.mean))
-        print("----------------------")
+        log.info("Candidate mean: {}".format(candidate_mean))
+        log.info("Baseline mean: {}".format(self.mean))
         if candidate_mean - self.mean < 0:
             # Calc p value
             t, p = ttest_rel(candidate_vals, self.bl_vals)
 
             p_val = p / 2  # one-sided
             assert t < 0, "T-statistic should be negative"
-            print("\np-value: {}".format(p_val))
+            log.info("\np-value: {}".format(p_val))
             if p_val < self.bl_alpha:
-                print("\nUpdating baseline")
-                self._update_model(model, dl, env)
+                log.info("\nUpdating baseline")
+                self._update_model(model, dl, env, device)
 
-    def rollout(self, model, dl, env=None):
+    def rollout(self, model, dl, env=None, device="cpu"):
         """
         Rollout the model on the given dataset.
         """
         env_fn = lambda x: x if env is None else env.reset(init_observation=x)
         with torch.no_grad():
             model.eval()
-            device = next(
-                model.parameters()
-            ).device  # same as transfer_batch_to_device in lightning, may be cleaner but hey
+            model = model.to(device)
             rewards = [
                 model(env_fn(batch.to(device)), decode_type="greedy")["reward"]
                 for batch in dl
