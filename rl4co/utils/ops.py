2fd93af02bd29103e0e320d56720617f4c2bc3d2
Federico Berto <berto.federico2@gmail.com>
2023-04-20 03:47:11 +0900

    [BugFix] batchify/unbatchify

--- rl4co/utils/ops.py
+++ rl4co/utils/ops.py
@@ -1,16 +1,19 @@
-def repeat_batch(x, repeats):
-    """Same as repeat on dim=0 for tensordicts as well
-    Same as einops.repeat(x, 'b n d -> (r b) n d', r=repeats) but 50% faster
+from typing import Union
+from tensordict import TensorDict
+from torch import Tensor
+
+
+def batchify(x: Union[Tensor, TensorDict], repeats: int) -> Union[Tensor, TensorDict]:
+    """Same as repeat on dim=0 for Tensordicts as well
+    Same as einops.repeat(x, 'b ... -> (b r) ...', r=repeats) but ~1.5x faster
     """
     s = x.shape
     return x.expand(repeats, *s).contiguous().view(s[0] * repeats, *s[1:])
 
 
-def undo_repeat_batch(x, repeats, dim=0):
-    """Undoes repeat_batch
-    Same as einops.rearrange(x, '(r b) ... -> r b ...', r=repeats) but 3x faster
+def unbatchify(x: Union[Tensor, TensorDict], repeats: int) -> Union[Tensor, TensorDict]:
+    """Undoes repeat_batch operation for Tensordicts as well
+    Same as einops.rearrange(x, '(r b) ... -> b r ...', r=repeats) but ~2x faster
     """
     s = x.shape
-    return x.view(
-        repeats, s[dim] // repeats, *[s[i] for i in range(len(s)) if i != dim]
-    )
+    return x.view(repeats, s[0] // repeats, *s[1:]).permute(1, 0, *range(2, len(s)+1))
