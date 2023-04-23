5e45d392a27073cb7cfad85db0c87a13d16b49a1
Federico Berto <berto.federico2@gmail.com>
2023-04-23 18:22:15 +0900

    [Tests] add better environment testing

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,10 +1,30 @@
 import pytest
+import torch
 
-from rl4co.envs import TSPEnv
+from rl4co.envs import TSPEnv, ATSPEnv 
 
 
-@pytest.mark.parametrize("size", [10, 50])
+@pytest.mark.parametrize("size", [10])
 def test_tsp(size):
     env = TSPEnv(num_loc=size)
-    a = env.reset()
-    assert a["observation"].shape == (size, 2)
+    bs = 2
+    actions = torch.stack([torch.randperm(size) for _ in range(bs)])
+    td = env.reset(batch_size=[bs])
+    for i in range(size):
+        td.set("action", actions[:, i][:, None])
+        td = env.step(td)['next']
+    reward = env.get_reward(td, actions)
+    assert reward.shape == (bs,)
+    
+
+@pytest.mark.parametrize("size", [10])
+def test_atsp(size):
+    env = ATSPEnv(num_loc=size)
+    bs = 2
+    actions = torch.stack([torch.randperm(size) for _ in range(bs)])
+    td = env.reset(batch_size=[bs])
+    for i in range(size):
+        td.set("action", actions[:, i][:, None])
+        td = env.step(td)['next']
+    reward = env.get_reward(td, actions)
+    assert reward.shape == (bs,)
