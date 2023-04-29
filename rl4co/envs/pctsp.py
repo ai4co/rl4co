f0fe83df01aed8d93cb3895d9503dbdc2e1f2495
Chuanbo Hua <cbhua@kaist.ac.kr>
2023-04-30 02:45:12 +0900

    [Feature] init pctsp environment

---
+++ rl4co/envs/pctsp.py
@@ -0,0 +1,272 @@
+import sys
+
+sys.path.append(".")
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from torch import Tensor
+from tensordict.tensordict import TensorDict
+from typing import Optional, Union
+
+from torchrl.data import (
+    BoundedTensorSpec,
+    CompositeSpec,
+    UnboundedContinuousTensorSpec,
+    UnboundedDiscreteTensorSpec,
+)
+
+from rl4co.envs import RL4COEnvBase
+
+
+class PCTSPEnv(RL4COEnvBase):
+    name = "pctsp"
+
+    def __init__(
+        self,
+        num_loc: int = 10,
+        min_loc: float = 0,
+        max_loc: float = 1,
+        min_prize: float = 0.1,
+        max_prize: float = 0.5,
+        min_penalty: float = 0.1,
+        max_penalty: float = 0.5,
+        require_prize: float = 1,
+        batch_size: list = [],
+        td_params: TensorDict = None,
+        seed: int = None,
+        device: str = "cpu",
+    ):
+        """
+        Capacity Vehicle Routing Problem (CVRP) environment
+        At each step, the agent chooses a city to visit. The reward is the -infinite unless the agent visits all the cities.
+        In that case, the reward is (-)length of the path: maximizing the reward is equivalent to minimizing the path length.
+
+        Args:
+            - num_loc <int>: number of locations (cities) in the VRP. NOTE: the depot is included
+            - min_loc <float>: minimum value for the location coordinates
+            - max_loc <float>: maximum value for the location coordinates
+            - capacity <float>: capacity of the vehicle
+            - td_params <TensorDict>: parameters of the environment
+            - seed <int>: seed for the environment
+            - device <str>: 'cpu' or 'cuda:0', device to use.  Generally, no need to set as tensors are updated on the fly
+        """
+        super().__init__(seed=seed, device=device)
+        self.num_loc = num_loc
+        self.min_loc = min_loc
+        self.max_loc = max_loc
+        self.min_prize = min_prize
+        self.max_prize = max_prize
+        self.min_penalty = min_penalty
+        self.max_penalty = max_penalty
+        self.require_prize = require_prize
+        self.batch_size = batch_size
+        self._make_spec(td_params)
+
+    @staticmethod
+    def _step(td: TensorDict) -> TensorDict:
+        """Update the states of the environment
+        Args:
+            - td <TensorDict>: tensor dictionary containing with the action
+                - action <int> [batch_size, 1]: action to take
+        NOTE:
+            - the first node in de demand is larger than 0 or less than 0?
+            - this design is important. For now the design is LESS than 0
+        """
+        current_node = td["action"][..., None]
+        prize = td["prize"]
+        prize_collect = td["prize_collect"]
+
+        # Collect the prize
+        prize_collect += torch.gather(td["prize"], 1, current_node).squeeze()
+
+        # Update the prize
+        prize.scatter_(-1, current_node, 0)
+
+        # Get the action mask, no zero demand nodes can be visited
+        action_mask = prize > 0
+
+        # We are done there are no unvisited locations
+        # NOTE: in the PCTSP problem, we can finish at any time 
+        done = torch.logical_and(torch.count_nonzero(prize, dim=-1) <= 0, prize_collect >= td["prize_require"])
+
+        # Calculate reward (minus length of path, since we want to maximize the reward -> minimize the path length)
+        # Note: reward is calculated outside for now via the get_reward function
+        # to calculate here need to pass action sequence or save it as state
+        reward = torch.ones_like(done) * float("-inf")
+
+        # The output must be written in a ``"next"`` entry
+        return TensorDict(
+            {
+                "next": {
+                    "observation": td["observation"],
+                    "current_node": current_node,
+                    "prize": prize,
+                    "prize_collect": prize_collect,
+                    "prize_require": td["prize_require"],
+                    "penalty": td["penalty"],
+                    "action_mask": action_mask,
+                    "reward": reward,
+                    "done": done,
+                }
+            },
+            td.shape,
+        )
+
+    def _reset(
+        self, td: Optional[TensorDict] = None, batch_size: Optional[list] = None
+    ) -> TensorDict:
+        """
+        Args:
+            - td (Optional) <TensorDict>: tensor dictionary containing the initial state
+        """
+        if batch_size is None:
+            batch_size = self.batch_size if td is None else td["observation"].shape[:-2]
+
+        if td is None or td.is_empty():
+            td = self.generate_data(batch_size=batch_size)
+
+        # Initialize the current node
+        current_node = torch.zeros(
+            (*batch_size, 1), dtype=torch.int64, device=self.device
+        )
+
+        # Collected prize
+        prize_collect = torch.zeros(
+            (*batch_size, 1), dtype=torch.float32, device=self.device
+        )
+
+        # Required prize
+        prize_require = torch.full(
+            (*batch_size, 1), self.prize_require, dtype=torch.float32, device=self.device
+        )
+
+        # Init the action mask
+        action_mask = td["prize"] > 0
+
+        return TensorDict(
+            {
+                "observation": td["observation"],
+                "current_node": current_node,
+                "prize": td["prize"],
+                "prize_collect": prize_collect,
+                "prize_require": prize_require,
+                "penalty": td["penalty"],
+                "action_mask": action_mask,
+            },
+            batch_size=batch_size,
+        )
+
+    def _make_spec(self, td_params: TensorDict):
+        """Make the observation and action specs from the parameters."""
+        self.observation_spec = CompositeSpec(
+            observation=BoundedTensorSpec(
+                minimum=self.min_loc,
+                maximum=self.max_loc,
+                shape=(self.num_loc, 2),
+                dtype=torch.float32,
+            ),
+            current_node=UnboundedDiscreteTensorSpec(
+                shape=(1),
+                dtype=torch.int64,
+            ),
+            prize=BoundedTensorSpec(
+                minimum=self.min_prize,
+                maximum=self.max_prize,
+                shape=(self.num_loc, 1),
+                dtype=torch.float32,
+            ),
+            prize_collect=UnboundedContinuousTensorSpec(
+                shape=(1,),
+                dtype=torch.float32,
+            ),
+            action_mask=UnboundedDiscreteTensorSpec(
+                shape=(self.num_loc, 1),
+                dtype=torch.bool,
+            ),
+            shape=(),
+        )
+        self.input_spec = self.observation_spec.clone()
+        self.action_spec = BoundedTensorSpec(
+            shape=(1,),
+            dtype=torch.int64,
+            minimum=0,
+            maximum=self.num_loc,
+        )
+        self.reward_spec = UnboundedContinuousTensorSpec(shape=(1,))
+        self.done_spec = UnboundedDiscreteTensorSpec(shape=(1,), dtype=torch.bool)
+
+    @staticmethod
+    def get_reward(td, actions) -> TensorDict:
+        """
+        Args:
+            - td: <tensor_dict>: tensor dictionary containing the state
+            - actions: [batch_size, TODO] num_loc means a sequence of actions till the task is done
+        NOTE:
+            - about the length of the actions
+        """
+        # Calculate the length
+        locs = td["observation"]
+        locs = locs.gather(1, actions[..., None].expand(*actions.size(), locs.size(-1)))
+        locs_next = torch.roll(locs, 1, dims=1)
+        length = -((locs_next - locs).norm(p=2, dim=2).sum(1))
+
+        # Calculate the penalty
+        penalty = td["penalty"] * (td['prize'] > 0).float()
+        return length + penalty
+
+    def generate_data(self, batch_size) -> TensorDict:
+        """
+        Args:
+            - batch_size <int> or <list>: batch size
+        Returns:
+            - td <TensorDict>: tensor dictionary containing the initial state
+                - observation <Tensor> [batch_size, num_loc, 2]: locations of the nodes
+                - demand <Tensor> [batch_size, num_loc]: demand of the nodes
+                - capacity <Tensor> [batch_size, 1]: capacity of the vehicle
+                - current_node <Tensor> [batch_size, 1]: current node
+                - i <Tensor> [batch_size, 1]: number of visited nodes
+        NOTE:
+            - the observation includes the depot as the first node
+            - the demand includes the used capacity at the first value
+            - the unvisited variable can be replaced by demand > 0
+        """
+        # Batch size input check
+        batch_size = [batch_size] if isinstance(batch_size, int) else batch_size
+
+        # Initialize the locations (including the depot which is always the first node)
+        locs = (
+            torch.FloatTensor(*batch_size, self.num_loc, 2)
+            .uniform_(self.min_loc, self.max_loc)
+            .to(self.device)
+        )
+
+        # Initialize the prize
+        prize = (
+            torch.FloatTensor(*batch_size, self.num_loc)
+            .uniform_(self.min_prize, self.max_prize)
+            .to(self.device)
+        )
+
+        # Initialize the penalty
+        penalty = (
+            torch.FloatTensor(*batch_size, self.num_loc)
+            .uniform_(self.min_penalty, self.max_penalty)
+            .to(self.device)
+        )
+
+        # The depot prize and penalty are zero
+        prize[..., 0] = 0
+        penalty[..., 0] = 0
+
+        return TensorDict(
+            {
+                "observation": locs,
+                "depot": locs[..., 0, :],
+                "prize": prize,
+                "penalty": penalty,
+            },
+            batch_size=batch_size,
+        )
+
+    def render(self, td: TensorDict):
+        raise NotImplementedError("TODO: render is not implemented yet")
