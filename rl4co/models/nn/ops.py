da4df20205743c4e3c5cd903552048d502dc78a1
Federico Berto <berto.federico2@gmail.com>
2023-05-10 02:10:56 +0900

    [Refactor] move ops from graph

---
+++ rl4co/models/nn/ops.py
@@ -0,0 +1,38 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+class SkipConnection(nn.Module):
+    def __init__(self, module):
+        super(SkipConnection, self).__init__()
+        self.module = module
+
+    def forward(self, x):
+        return x + self.module(x)
+
+
+class Normalization(nn.Module):
+    def __init__(self, embed_dim, normalization="batch"):
+        super(Normalization, self).__init__()
+
+        normalizer_class = {"batch": nn.BatchNorm1d, "instance": nn.InstanceNorm1d}.get(
+            normalization, None
+        )
+
+        self.normalizer = normalizer_class(embed_dim, affine=True)
+
+    def init_parameters(self):
+        for name, param in self.named_parameters():
+            stdv = 1.0 / math.sqrt(param.size(-1))
+            param.data.uniform_(-stdv, stdv)
+
+    def forward(self, x):
+        if isinstance(self.normalizer, nn.BatchNorm1d):
+            return self.normalizer(x.view(-1, x.size(-1))).view(*x.size())
+        elif isinstance(self.normalizer, nn.InstanceNorm1d):
+            return self.normalizer(x.permute(0, 2, 1)).permute(0, 2, 1)
+        else:
+            assert self.normalizer is None, "Unknown normalizer type"
+            return x
