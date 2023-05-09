9f4eef52e789c065ef3cd5f6b9c8f4a30701437f
Federico Berto <berto.federico2@gmail.com>
2023-05-10 02:10:04 +0900

    [Refactor] better testing

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,66 +1,62 @@
 import pytest
 import torch
 
-from rl4co.envs import TSPEnv, ATSPEnv, DPPEnv, CVRPEnv, SDVRPEnv
+from rl4co.envs import TSPEnv, ATSPEnv, DPPEnv, CVRPEnv, SDVRPEnv, PDPEnv
+
+
+def policy(td):
+    """Helper function to select a random action from available actions"""
+    action = torch.multinomial(td['action_mask'].float(), 1).squeeze(-1)
+    td.set("action", action)
+    return td
+
+
+def rollout(env, td, policy):
+    """Helper function to rollout a policy"""
+    actions = []
+    while not td["done"].any():
+        td = policy(td)
+        actions.append(td["action"])
+        td = env.step(td)["next"]
+    return env.get_reward(td, torch.stack(actions, dim=1))
 
 
 @pytest.mark.parametrize("size, batch_size", [(10, 2)])
 def test_tsp(size, batch_size):
     env = TSPEnv(num_loc=size)
-    actions = torch.stack([torch.randperm(size) for _ in range(batch_size)])
-    td = env.reset(batch_size=[batch_size])
-    for i in range(size):
-        td.set("action", actions[:, i])
-        td = env.step(td)["next"]
-    reward = env.get_reward(td, actions)
+    reward = rollout(env, env.reset(batch_size=[batch_size]), policy)
     assert reward.shape == (batch_size,)
 
 
 @pytest.mark.parametrize("size, batch_size", [(10, 2)])
 def test_atsp(size, batch_size):
     env = ATSPEnv(num_loc=size)
-    actions = torch.stack([torch.randperm(size) for _ in range(batch_size)])
-    td = env.reset(batch_size=[batch_size])
-    for i in range(size):
-        td.set("action", actions[:, i])
-        td = env.step(td)["next"]
-    reward = env.get_reward(td, actions)
+    reward = rollout(env, env.reset(batch_size=[batch_size]), policy)
     assert reward.shape == (batch_size,)
 
 
 @pytest.mark.parametrize("size, batch_size", [(10, 2)])
 def test_dpp(size, batch_size):
     env = DPPEnv()
-    actions = torch.stack([torch.randperm(size) for _ in range(batch_size)])
-    td = env.reset(batch_size=[batch_size])
-    for i in range(size):
-        td.set("action", actions[:, i])
-        td = env.step(td)["next"]
-    reward = env.get_reward(td, actions)
+    reward = rollout(env, env.reset(batch_size=[batch_size]), policy)
     assert reward.shape == (batch_size,)
 
 
 @pytest.mark.parametrize("size, batch_size", [(10, 2)])
 def test_cvrp(size, batch_size):
-    # NOTE: we may need to select random actions in a different way
     env = CVRPEnv()
-    actions = torch.stack([torch.randperm(size) for _ in range(batch_size)])
-    td = env.reset(batch_size=[batch_size])
-    for i in range(size):
-        td.set("action", actions[:, i])
-        td = env.step(td)["next"]
-    reward = env.get_reward(td, actions)
+    reward = rollout(env, env.reset(batch_size=[batch_size]), policy)
     assert reward.shape == (batch_size,)
 
 
 @pytest.mark.parametrize("size, batch_size", [(10, 2)])
 def test_sdvrp(size, batch_size):
-    # NOTE: we may need to select random actions in a different way
     env = SDVRPEnv()
-    actions = torch.stack([torch.randperm(size) for _ in range(batch_size)])
-    td = env.reset(batch_size=[batch_size])
-    for i in range(size):
-        td.set("action", actions[:, i])
-        td = env.step(td)["next"]
-    reward = env.get_reward(td, actions)
+    reward = rollout(env, env.reset(batch_size=[batch_size]), policy)
+    assert reward.shape == (batch_size,)
+
+@pytest.mark.parametrize("size, batch_size", [(10, 2)])
+def test_pdp(size, batch_size):
+    env = PDPEnv(num_loc=size)
+    reward = rollout(env, env.reset(batch_size=[batch_size]), policy)
     assert reward.shape == (batch_size,)
