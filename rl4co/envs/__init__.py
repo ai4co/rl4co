71bcf626f941fc1fd280cdb045c7933ee5525b65
Federico Berto <berto.federico2@gmail.com>
2023-05-10 02:11:53 +0900

    [Feature] add PDP

--- rl4co/envs/__init__.py
+++ rl4co/envs/__init__.py
@@ -4,3 +4,4 @@ from rl4co.envs.dpp import DPPEnv
 from rl4co.envs.atsp import ATSPEnv
 from rl4co.envs.cvrp import CVRPEnv
 from rl4co.envs.sdvrp import SDVRPEnv
+from rl4co.envs.pdp import PDPEnv
---
+++ rl4co/envs/pdp.py
@@ -0,0 +1,239 @@
+from typing import Optional
+
+import numpy as np
+import torch
+from tensordict.tensordict import TensorDict
+
+from torchrl.data import (
+    BoundedTensorSpec,
+    CompositeSpec,
+    UnboundedContinuousTensorSpec,
+    UnboundedDiscreteTensorSpec,
+)
+
+from rl4co.envs.utils import batch_to_scalar
+from rl4co.envs.base import RL4COEnvBase
+
+
+class PDPEnv(RL4COEnvBase):
+    name = "pdp"
+
+    def __init__(
+        self,
+        num_loc: int = 20,
+        min_loc: float = 0,
+        max_loc: float = 1,
+        td_params: TensorDict = None,
+        seed: int = None,
+        device: str = "cpu",
+    ):
+        """
+        Traveling Salesman Problem environment
+        At each step, the agent chooses a city to visit. The reward is the -infinite unless the agent visits all the cities.
+        In that case, the reward is (-)length of the path: maximizing the reward is equivalent to minimizing the path length.
+
+        Args:
+            num_loc: number of locations (cities) in the TSP
+            td_params: parameters of the environment
+            seed: seed for the environment
+            device: device to use.  Generally, no need to set as tensors are updated on the fly
+        """
+        super().__init__(seed=seed, device=device)
+        self.num_loc = num_loc
+        self.min_loc = min_loc
+        self.max_loc = max_loc
+        self._make_spec(td_params)
+
+    @staticmethod
+    def _step(td: TensorDict) -> TensorDict:
+        current_node = td["action"].unsqueeze(-1)
+
+        num_loc = td['locs'].shape[-2] - 1 # except depot
+
+        # Pickup and delivery node pair of selected node
+        new_to_deliver = (current_node +  num_loc // 2) % (num_loc + 1)
+        
+        # Set available to 0 (i.e., we visited the node)
+        available = td["available"].scatter(
+            -1, current_node.expand_as(td["action_mask"]), 0
+        )
+
+        to_deliver = td["to_deliver"].scatter(
+            -1, new_to_deliver.expand_as(td["to_deliver"]), 1
+        )
+
+        # Action is feasible if the node is not visited and is to deliver
+        action_mask = torch.logical_and(available, to_deliver)
+
+        # We are done there are no unvisited locations
+        done = torch.count_nonzero(available, dim=-1) == 0
+
+        # The reward is calculated outside via get_reward for efficiency, so we set it to -inf here
+        reward = torch.ones_like(done) * float("-inf")
+
+        # The output must be written in a ``"next"`` entry
+        return TensorDict(
+            {
+                "next": {
+                    "locs": td["locs"],
+                    "current_node": current_node,
+                    "available": available,
+                    "to_deliver": to_deliver,
+                    "i": td["i"] + 1,
+                    "action_mask": action_mask,
+                    "reward": reward,
+                    "done": done,
+                }
+            },
+            td.shape,
+        )
+
+    def _reset(self, td: Optional[TensorDict] = None, batch_size=None) -> TensorDict:
+        # Initialize locations
+        init_locs = td["locs"] if td is not None else None
+        if batch_size is None:
+            batch_size = self.batch_size if init_locs is None else init_locs.shape[:-2]
+        self.device = device = (
+            init_locs.device if init_locs is not None else self.device
+        )
+        if init_locs is None:
+            init_locs = self.generate_data(batch_size=batch_size)['locs'].to(device)
+
+        # Pick is 1, deliver is 0 [batch_size, graph_size+1], [1,1...1, 0...0]
+        to_deliver = torch.cat([torch.ones(*batch_size, self.num_loc // 2 + 1, dtype=torch.bool, device=device),
+            torch.zeros(*batch_size, self.num_loc // 2, dtype=torch.bool, device=device)], dim=-1) 
+        
+        # Cannot visit depot at first step # [0,1...1] so set not available
+        available = torch.ones(
+            (*batch_size, self.num_loc + 1), dtype=torch.bool, device=device
+        )
+        action_mask = ~available.contiguous() # [batch_size, graph_size+1]
+        action_mask[..., 0] = 1 # First step is always the depot
+
+        # Other variables
+        current_node = torch.zeros((*batch_size, 1), dtype=torch.int64, device=device)
+        i = torch.zeros((*batch_size, 1), dtype=torch.int64, device=device)
+
+        return TensorDict(
+            {
+                "locs": init_locs, # init_locs [..., 0] is depot
+                "current_node": current_node,
+                "to_deliver": to_deliver,
+                "available": available,
+                "i": i,
+                "action_mask": action_mask,
+            },
+            batch_size=batch_size,
+        )
+
+    def _make_spec(self, td_params: TensorDict):
+        """Make the observation and action specs from the parameters."""
+        self.observation_spec = CompositeSpec(
+            locs=BoundedTensorSpec(
+                minimum=self.min_loc,
+                maximum=self.max_loc,
+                shape=(self.num_loc, 2),
+                dtype=torch.float32,
+            ),
+            current_node=UnboundedDiscreteTensorSpec(
+                shape=(1),
+                dtype=torch.int64,
+            ),
+            to_deliver=UnboundedDiscreteTensorSpec(
+                shape=(1),
+                dtype=torch.int64,
+            ),
+            i=UnboundedDiscreteTensorSpec(
+                shape=(1),
+                dtype=torch.int64,
+            ),
+            action_mask=UnboundedDiscreteTensorSpec(
+                shape=(self.num_loc),
+                dtype=torch.bool,
+            ),
+            shape=(),
+        )
+        self.input_spec = self.observation_spec.clone()
+        self.action_spec = BoundedTensorSpec(
+            shape=(1,),
+            dtype=torch.int64,
+            minimum=0,
+            maximum=self.num_loc,
+        )
+        self.reward_spec = UnboundedContinuousTensorSpec(shape=(1,))
+        self.done_spec = UnboundedDiscreteTensorSpec(shape=(1,), dtype=torch.bool)
+
+    def get_reward(self, td, actions) -> TensorDict:        
+        assert (actions[:, 0]==0).all(), "Not starting at depot"
+        assert (torch.arange(actions.size(1), out=actions.data.new()).view(1, -1).expand_as(actions) == actions.data.sort(1)[0]).all(), "Not visiting all nodes"
+
+        visited_time = torch.argsort(actions, 1)  # index of pickup less than index of delivery 
+        assert (visited_time[:, 1:actions.size(1) // 2 + 1] < visited_time[:, actions.size(1) // 2 + 1:]).all(), "Deliverying without pick-up"
+        
+        # Gather locations in the order of actions and get reward = -(total distance)
+        locs = td['locs'].gather(1, actions.unsqueeze(-1).expand_as(td['locs']))  # [batch, graph_size+1, 2]
+        locs_next = torch.roll(locs, 1, dims=1) 
+        return -(locs_next - locs).norm(p=2, dim=2).sum(1)
+
+    def generate_data(self, batch_size) -> TensorDict:
+        batch_size = [batch_size] if isinstance(batch_size, int) else batch_size
+
+        # Initialize the locations (including the depot which is always the first node)
+        locs = (
+            torch.FloatTensor(*batch_size, self.num_loc + 1, 2)
+            .uniform_(self.min_loc, self.max_loc)
+            .to(self.device)
+        )
+        return TensorDict({"locs": locs}, batch_size=batch_size,)
+
+    @staticmethod
+    def render(td, actions):
+        import matplotlib.pyplot as plt
+        markersize = 8
+
+        td = td.detach().cpu()
+        # if batch_size greater than 0 , we need to select the first batch element
+        if td.batch_size != torch.Size([]):
+            td = td[0]
+            actions = actions[0]
+
+        # Variables
+        init_deliveries = td['to_deliver'][1:]
+        delivery_locs = td['locs'][1:][~init_deliveries.bool()]
+        pickup_locs = td['locs'][1:][init_deliveries.bool()]
+        depot_loc = td['locs'][0]
+
+        fig, ax = plt.subplots()
+
+        # Plot the actions in order
+        for i in range(len(actions)):
+            from_node = actions[i]
+            to_node = actions[i+1] if i < len(actions)-1 else actions[0] # last goes back to depot
+            from_loc = td['locs'][from_node]
+            to_loc = td['locs'][to_node]
+            ax.plot([from_loc[0], to_loc[0]], [from_loc[1], to_loc[1]], 'k-')
+            ax.annotate("", xy=(to_loc[0], to_loc[1]), xytext=(from_loc[0], from_loc[1]),
+                        arrowprops=dict(arrowstyle="->", color='black'), annotation_clip=False)
+
+        # Plot the depot location
+        ax.plot(depot_loc[0], depot_loc[1], "g", marker='s', markersize=markersize, label='Depot')
+
+        # Plot the pickup locations
+        for i, pickup_loc in enumerate(pickup_locs):
+            ax.plot(pickup_loc[0], pickup_loc[1], 'r', marker='^', markersize=markersize, label='Pickup' if i == 0 else None)
+
+        # Plot the delivery locations
+        for i, delivery_loc in enumerate(delivery_locs):
+            ax.plot(delivery_loc[0], delivery_loc[1], 'b', marker='v', markersize=markersize, label='Delivery' if i == 0 else None)
+
+        # Legend
+        # plt.legend(['Actions', 'Depot', 'Delivery', 'Pickup'])
+        # get handles
+        handles, labels = ax.get_legend_handles_labels()
+
+        # plot legend
+        ax.legend(handles, labels)
+        ax.set_title('Pickup and Delivery Problem Solution')
+        ax.set_xlabel('x-coordinate')
+        ax.set_ylabel('y-coordinate')
+        plt.show()
