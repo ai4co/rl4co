9a48a8fbd27d936f6f3283c091a1fe2f2a0cbba1
Federico Berto <berto.federico2@gmail.com>
2023-06-07 03:57:32 +0900

    [Scripts] update sample efficiency notebook

--- scripts/sample_efficiency.sh
+++ scripts/sample_efficiency.sh
@@ -1,26 +1,27 @@
-!#/bin/bash
+#!/bin/bash
 
 
 ##################################
 # RUN FOR ALL 
-# ONLY THING TO CHANGE
 # NUM_NODES: 20, 50, 100
 # ENV: tsp, cvrp
 # EXP_NAME: am-critic, am, pomo, symnco
 
-NUM_NODES=20
+# 1 epoch: 20k samples for all models!!
+# 100 epochs: 20k x 100 = 2M samples
+# we want to normalize the number of samples AND gradient steps
+
+NUM_NODES=50
 ENV=tsp
 EXP_NAME='am-critic'
 export CUDA_VISIBLE_DEVICES=0 # change device id
 
+# DO NOT CHANGE
+MAX_EPOCHS=100 
+
 ##################################
 
 
-
-
-# 1 epoch: 50k samples for all models!!
-# 40 epochs: 50k x 40 = 2M samples
-# we want to normalize the number of samples AND gradient steps
 for seed in 1234, 1235, 1236;
     do
 
@@ -31,10 +32,10 @@ for seed in 1234, 1235, 1236;
         env.num_loc=${NUM_NODES} \
         seed=$seed \
         logger.wandb.name=am-critic-${ENV}-${NUM_NODES} \
-        logger.wandb.project=rl4co-sample-efficiency \
-        data.train_size=50000 \
+        logger.wandb.project=rl4co-sample-efficiency2 \
+        data.train_size=20000 \
         data.batch_size=500 \
-        trainer.max_epochs=40
+        trainer.max_epochs=${MAX_EPOCHS}
     elif [ $EXP_NAME == 'am' ]; then
         # AM ( we have x2 samples, so less samples per epoch
         # 20, 50, 100 nodes 
@@ -42,10 +43,10 @@ for seed in 1234, 1235, 1236;
         env.num_loc=${NUM_NODES} \
         seed=$seed \
         logger.wandb.name=am-critic-${ENV}-${NUM_NODES} \
-        logger.wandb.project=rl4co-sample-efficiency \
-        data.train_size=25000 \
+        logger.wandb.project=rl4co-sample-efficiency2 \
+        data.train_size=10000 \
         data.batch_size=250 \
-        trainer.max_epochs=40
+        trainer.max_epochs=${MAX_EPOCHS}
     elif [ $EXP_NAME == 'pomo' ]; then
 
         if [ $NUM_NODES == 20 ]; then
@@ -54,30 +55,30 @@ for seed in 1234, 1235, 1236;
             env.num_loc=${NUM_NODES} \
             seed=$seed \
             logger.wandb.name=pomo-${ENV}-${NUM_NODES} \
-            logger.wandb.project=rl4co-sample-efficiency \
-            data.train_size=50000 \
+            logger.wandb.project=rl4co-sample-efficiency2 \
+            data.train_size=1000 \
             data.batch_size=25 \
-            trainer.max_epochs=40
+            trainer.max_epochs=${MAX_EPOCHS}
         elif [ $NUM_NODES == 50 ]; then
             # POMO (50 nodes)
             python run.py experiment=${ENV}/pomo \
             env.num_loc=${NUM_NODES} \
             seed=$seed \
             logger.wandb.name=pomo-${ENV}-${NUM_NODES} \
-            logger.wandb.project=rl4co-sample-efficiency \
-            data.train_size=50000 \
+            logger.wandb.project=rl4co-sample-efficiency2 \
+            data.train_size=400 \
             data.batch_size=10 \
-            trainer.max_epochs=40
+            trainer.max_epochs=${MAX_EPOCHS}
         elif [ $NUM_NODES == 100 ]; then
             # POMO (100 nodes)
             python run.py experiment=${ENV}/pomo \
             env.num_loc=${NUM_NODES} \
             seed=$seed \
             logger.wandb.name=pomo-${ENV}-${NUM_NODES} \
-            logger.wandb.project=rl4co-sample-efficiency \
-            data.train_size=50000 \
+            logger.wandb.project=rl4co-sample-efficiency2 \
+            data.train_size=200 \
             data.batch_size=5 \
-            trainer.max_epochs=40
+            trainer.max_epochs=${MAX_EPOCHS}
         fi
 
     elif [ $EXP_NAME == 'symnco' ]; then
@@ -87,9 +88,9 @@ for seed in 1234, 1235, 1236;
         env.num_loc=${NUM_NODES} \
         seed=$seed \
         logger.wandb.name=symnco-${ENV}-${NUM_NODES} \
-        logger.wandb.project=rl4co-sample-efficiency \
-        data.train_size=50000 \
+        logger.wandb.project=rl4co-sample-efficiency2 \
+        data.train_size=2000 \
         data.batch_size=50 \
-        trainer.max_epochs=40
+        trainer.max_epochs=${MAX_EPOCHS}
     fi
 done
