6c63cb6c18cb8fb1b7407e670892255b97b2ae86
Federico Berto <berto.federico2@gmail.com>
2023-06-07 10:14:59 +0900

    [Minor] name fix

--- scripts/sample_efficiency.sh
+++ scripts/sample_efficiency.sh
@@ -42,7 +42,7 @@ for seed in 1234, 1235, 1236;
         python run.py experiment=${ENV}/am \
         env.num_loc=${NUM_NODES} \
         seed=$seed \
-        logger.wandb.name=am-critic-${ENV}-${NUM_NODES} \
+        logger.wandb.name=am-${ENV}-${NUM_NODES} \
         logger.wandb.project=rl4co-sample-efficiency2 \
         data.train_size=10000 \
         data.batch_size=250 \
