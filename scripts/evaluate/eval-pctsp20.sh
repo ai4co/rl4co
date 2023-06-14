18b9ae2de34e74c446a9b94853537edad27ba970
Federico Berto <berto.federico2@gmail.com>
2023-06-14 19:42:34 +0900

    [Scripts] update

---
+++ scripts/evaluate/eval-pctsp20.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RUN_CMD="python scripts/eval_methods.py"
+
+################
+# CONFIGS
+ENV="pctsp"
+SIZE=20
+TASK="${ENV}${SIZE}"
+GPU_ID=3
+################
+
+${RUN_CMD} --dir "saved_checkpoints2/${TASK}/am-${TASK}" --gpu_id ${GPU_ID} --checkpoint "last.ckpt"
+${RUN_CMD} --dir "saved_checkpoints2/${TASK}/pomo-${TASK}" --gpu_id ${GPU_ID} --checkpoint "last.ckpt"
+${RUN_CMD} --dir "saved_checkpoints2/${TASK}/symnco-${TASK}" --gpu_id ${GPU_ID} --checkpoint "last.ckpt"
+${RUN_CMD} --dir "saved_checkpoints2/${TASK}/am-${TASK}-sm-xl" --gpu_id ${GPU_ID} --checkpoint "last.ckpt"
