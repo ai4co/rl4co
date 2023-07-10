77b6ef21072b0bbaf5f0415a8a9fc82fa7ac28e8
Federico Berto <berto.federico2@gmail.com>
2023-07-11 03:48:25 +0900

    [Feat] PCTSP plotting

--- rl4co/envs/pctsp.py
+++ rl4co/envs/pctsp.py
@@ -15,7 +15,6 @@ from rl4co.envs.common.base import RL4COEnvBase
 from rl4co.utils.ops import gather_by_index, get_tour_length
 from rl4co.utils.pylogger import get_pylogger
 
-
 log = get_pylogger(__name__)
 
 # For the penalty to make sense it should be not too large (in which case all nodes will be visited) nor too small
@@ -42,7 +41,7 @@ class PCTSPEnv(RL4COEnvBase):
         check_solution (bool): Set to False by default for small bug happening around 0.01% of the time (TODO: fix)
         td_params (TensorDict): Parameters of the environment
     """
-    
+
     name = "pctsp"
     _stochastic = False
 
@@ -75,7 +74,7 @@ class PCTSPEnv(RL4COEnvBase):
         cur_total_penalty = td["cur_total_penalty"] + gather_by_index(
             td["penalty"], current_node
         )
-        
+
         # Update visited
         visited = td["visited"].scatter(-1, current_node[..., None], 1)
 
@@ -103,7 +102,7 @@ class PCTSPEnv(RL4COEnvBase):
             batch_size=td.batch_size,
         )
         td_step["next"].set("action_mask", self.get_action_mask(td_step["next"]))
-        return td_step 
+        return td_step
 
     def _reset(
         self, td: Optional[TensorDict] = None, batch_size: Optional[list] = None
@@ -116,7 +115,9 @@ class PCTSPEnv(RL4COEnvBase):
 
         locs = torch.cat([td["depot"][..., None, :], td["locs"]], dim=-2)
         expected_prize = td["deterministic_prize"]
-        real_prize = td["stochastic_prize"] if self.stochastic else td["deterministic_prize"]
+        real_prize = (
+            td["stochastic_prize"] if self.stochastic else td["deterministic_prize"]
+        )
         penalty = td["penalty"]
 
         # Concatenate depots
@@ -158,7 +159,7 @@ class PCTSPEnv(RL4COEnvBase):
         )
         td_reset.set("action_mask", self.get_action_mask(td_reset))
         return td_reset
-    
+
     @staticmethod
     def get_action_mask(td: TensorDict) -> torch.Tensor:
         """Cannot visit depot if not yet collected 1 total prize and there are unvisited nodes"""
@@ -172,10 +173,10 @@ class PCTSPEnv(RL4COEnvBase):
         """Reward is `saved penalties - (total length + penalty)`"""
 
         # In case all tours directly return to depot, prevent further problems
-        if actions.size(-1) == 1:  
+        if actions.size(-1) == 1:
             assert (actions == 0).all(), "If all length 1 tours, they should be zero"
             return torch.zeros(actions.size(0), dtype=torch.float, device=actions.device)
-        
+
         # Check that the solution is valid
         if self.check_solution:
             self.check_solution_validity(td, actions)
@@ -183,7 +184,7 @@ class PCTSPEnv(RL4COEnvBase):
         # Gather locations in order of tour and get the length of tours
         locs_ordered = gather_by_index(td["locs"], actions)
         length = get_tour_length(locs_ordered)
-        
+
         # Reward is saved penalties - (total length + penalty)
         saved_penalty = td["penalty"].gather(1, actions)
         return saved_penalty.sum(-1) - (length + td["penalty"][..., 1:].sum(-1))
@@ -196,7 +197,10 @@ class PCTSPEnv(RL4COEnvBase):
         sorted_actions = actions.data.sort(1)[0]
 
         # Make sure each node visited once at most (except for depot)
-        assert ((sorted_actions[..., 1:] == 0) | (sorted_actions[..., 1:] > sorted_actions[..., :-1])).all(), "Duplicates"
+        assert (
+            (sorted_actions[..., 1:] == 0)
+            | (sorted_actions[..., 1:] > sorted_actions[..., :-1])
+        ).all(), "Duplicates"
 
         prize = td["real_prize"][..., 1:]  # Remove depot
         prize_with_depot = torch.cat((torch.zeros_like(prize[:, :1]), prize), 1)
@@ -204,8 +208,11 @@ class PCTSPEnv(RL4COEnvBase):
 
         # Either prize constraint should be satisfied or all prizes should be visited
         assert (
-            (p.sum(-1) >= 1 - 1e-5) |
-            (sorted_actions.size(-1) - (sorted_actions == 0).int().sum(-1) == (td["locs"].size(-2) - 1)) # no depot
+            (p.sum(-1) >= 1 - 1e-5)
+            | (
+                sorted_actions.size(-1) - (sorted_actions == 0).int().sum(-1)
+                == (td["locs"].size(-2) - 1)
+            )  # no depot
         ).all(), "Total prize does not satisfy min total prize"
 
     def generate_data(self, batch_size) -> TensorDict:
@@ -257,12 +264,14 @@ class PCTSPEnv(RL4COEnvBase):
     @property
     def stochastic(self):
         return self._stochastic
-    
+
     @stochastic.setter
     def stochastic(self, state: bool):
-        if state == True:
-            log.warning('Stochastic mode should not be used for PCTSP. Use SPCTSP instead.')
-            
+        if state is True:
+            log.warning(
+                "Stochastic mode should not be used for PCTSP. Use SPCTSP instead."
+            )
+
     def _make_spec(self, td_params: TensorDict):
         """Make the locs and action specs from the parameters."""
         self.observation_spec = CompositeSpec(
@@ -325,5 +334,94 @@ class PCTSPEnv(RL4COEnvBase):
         self.done_spec = UnboundedDiscreteTensorSpec(shape=(1,), dtype=torch.bool)
 
     @staticmethod
-    def render(td: TensorDict, actions=None, ax=None):
-        raise NotImplementedError("TODO: render is not implemented yet")
+    def render(td, actions=None, ax=None):
+        import matplotlib.pyplot as plt
+        import numpy as np
+
+        from matplotlib import colormaps
+
+        # Create a plot of the nodes
+        if ax is None:
+            _, ax = plt.subplots()
+
+        td = td.detach().cpu()
+
+        # Actions
+        if actions is None:
+            actions = td.get("action", None)
+        actions = actions.detach().cpu() if actions is not None else None
+
+        # if batch_size greater than 0 , we need to select the first batch element
+        if td.batch_size != torch.Size([]):
+            td = td[0]
+            actions = actions[0] if actions is not None else None
+
+        # Variables
+        depot = td["locs"][0, :]
+        cities = td["locs"][1:, :]
+        prizes = td["real_prize"][1:]
+        penalties = td["penalty"][1:]
+        normalized_prizes = (
+            200 * (prizes - torch.min(prizes)) / (torch.max(prizes) - torch.min(prizes))
+            + 10
+        )
+        normalized_penalties = (
+            3
+            * (penalties - torch.min(penalties))
+            / (torch.max(penalties) - torch.min(penalties))
+        )
+
+        # Represent penalty with colormap and size of edges
+        penalty_cmap = colormaps.get_cmap("BuPu")
+        penalty_colors = penalty_cmap(normalized_penalties)
+
+        # Plot depot and cities with prize (size of nodes) and penalties (size of borders)
+        ax.scatter(
+            depot[0],
+            depot[1],
+            marker="s",
+            c="tab:green",
+            edgecolors="black",
+            zorder=1,
+            s=100,
+        )  # Plot depot as square
+        ax.scatter(
+            cities[:, 0],
+            cities[:, 1],
+            s=normalized_prizes,
+            c=normalized_prizes,
+            cmap="autumn_r",
+            alpha=1,
+            edgecolors=penalty_colors,
+            linewidths=normalized_penalties,
+        )  # Plot all cities with size and color indicating the prize
+
+        # Gather locs in order of action if available
+        if actions is None:
+            print("No action in TensorDict, rendering unsorted locs")
+        else:
+            # Reorder the cities and their corresponding prizes based on actions
+            tour = cities[actions - 1]  # subtract 1 to match Python's 0-indexing
+
+            # Append the depot at the beginning and the end of the tour
+            tour = np.vstack((depot, tour, depot))
+
+            # Use quiver to plot the tour
+            dx, dy = np.diff(tour[:, 0]), np.diff(tour[:, 1])
+            ax.quiver(
+                tour[:-1, 0],
+                tour[:-1, 1],
+                dx,
+                dy,
+                scale_units="xy",
+                angles="xy",
+                scale=1,
+                zorder=2,
+                color="black",
+                width=0.0035,
+            )
+
+        # Setup limits and show
+        ax.set_xlim(-0.05, 1.05)
+        ax.set_ylim(-0.05, 1.05)
+        plt.show()
