82dfe0f5cc31504a81767b6957f6f184aa67920a
junyoungpark <junyoungpark@kaist.ac.kr>
2023-07-13 21:34:01 +0900

    check integrity of optimizer and scheduler after they get instantiated

--- rl4co/algos/common/base.py
+++ rl4co/algos/common/base.py
@@ -2,7 +2,6 @@ from typing import Any, Union
 
 import torch
 import torch.nn as nn
-
 from lightning import LightningModule
 from torch.utils.data import DataLoader
 
@@ -140,12 +139,8 @@ class RL4COLitModule(LightningModule):
         self.train_dataset = self.wrap_dataset(
             self.env.dataset(self.data_cfg["train_dataset_size"], phase="train")
         )
-        self.val_dataset = self.env.dataset(
-            self.data_cfg["val_dataset_size"], phase="val"
-        )
-        self.test_dataset = self.env.dataset(
-            self.data_cfg["test_dataset_size"], phase="test"
-        )
+        self.val_dataset = self.env.dataset(self.data_cfg["val_dataset_size"], phase="val")
+        self.test_dataset = self.env.dataset(self.data_cfg["test_dataset_size"], phase="test")
 
         if hasattr(self.policy, "setup"):
             self.policy.setup(self)
@@ -169,8 +164,8 @@ class RL4COLitModule(LightningModule):
             )
         else:  # User-defined optimizer
             opt_cls = self._optimizer_name_or_cls
-            assert isinstance(optimizer, torch.optim.Optimizer)
             optimizer = opt_cls(self.policy.parameters(), **self.optimizer_kwargs)
+            assert isinstance(optimizer, torch.optim.Optimizer)
 
         # instantiate lr scheduler
         if self._lr_scheduler_name_or_cls is None:
@@ -183,8 +178,8 @@ class RL4COLitModule(LightningModule):
                 )
             else:  # User-defined scheduler
                 scheduler_cls = self._lr_scheduler_name_or_cls
-                assert isinstance(scheduler_cls, torch.optim.lr_scheduler.LRScheduler)
                 scheduler = scheduler_cls(optimizer, **self.lr_scheduler_kwargs)
+                assert isinstance(scheduler, torch.optim.lr_scheduler.LRScheduler)
             return [optimizer], {
                 "scheduler": scheduler,
                 "interval": self.lr_scheduler_interval,
@@ -194,9 +189,7 @@ class RL4COLitModule(LightningModule):
     def log_metrics(self, metric_dict: dict, phase: str):
         """Log metrics to logger and progress bar"""
         metrics = getattr(self, f"{phase}_metrics")
-        metrics = {
-            f"{phase}/{k}": v.mean() for k, v in metric_dict.items() if k in metrics
-        }
+        metrics = {f"{phase}/{k}": v.mean() for k, v in metric_dict.items() if k in metrics}
 
         log_on_step = self.log_on_step if phase == "train" else False
         on_epoch = False if phase == "train" else True
