cc29b08d8c52558fdbe730ff7f8efa3342006794
Federico Berto <berto.federico2@gmail.com>
2023-08-07 00:58:55 +0900

    [BugFix] POMO

--- rl4co/models/zoo/pomo/model.py
+++ rl4co/models/zoo/pomo/model.py
@@ -1,125 +1,113 @@
-from tensordict import TensorDict
+from typing import Any, Union
 
+import torch.nn as nn
+
+from rl4co.data.transforms import StateAugmentation
+from rl4co.envs.common.base import RL4COEnvBase
 from rl4co.models.rl.reinforce.reinforce import REINFORCE
-from rl4co.models.rl.reinforce.baselines import SharedBaseline
-from rl4co.models.zoo.pomo.augmentations import StateAugmentation
 from rl4co.models.zoo.pomo.policy import POMOPolicy
-from rl4co.utils.ops import gather_by_index, unbatchify
+from rl4co.utils.ops import gather_by_index, get_num_starts, unbatchify
 
 
 class POMO(REINFORCE):
     """POMO Model for neural combinatorial optimization based on REINFORCE
-    Based on Kwon et al. (2020) http://arxiv.org/abs/2010.16011
+    Based on Kwon et al. (2020) http://arxiv.org/abs/2010.16011.
 
     Args:
         env: TorchRL Environment
-        policy: Policy
-        baseline: REINFORCE Baseline
-        num_starts: Number of starting actions (POMO samples) (default: 10)
-        num_augment: Number of augmentations (default: 8)
+        policy: Policy to use for the algorithm
+        policy_kwargs: Keyword arguments for policy
+        baseline: Baseline to use for the algorithm. Note that POMO only supports shared baseline,
+            so we will throw an error if anything else is passed.
+        num_augment: Number of augmentations (used only for validation and test)
+        use_dihedral_8: Whether to use dihedral 8 augmentation
+        num_starts: Number of starts for multi-start. If None, use the number of available actions
+        **kwargs: Keyword arguments passed to the superclass
     """
 
     def __init__(
         self,
-        env,
-        policy=None,
-        baseline=None,
-        num_starts=10,
-        num_augment=8,
-        **policy_kwargs,
+        env: RL4COEnvBase,
+        policy: Union[nn.Module, POMOPolicy] = None,
+        policy_kwargs={},
+        baseline: str = "shared",
+        num_augment: int = 8,
+        use_dihedral_8: bool = True,
+        num_starts: int = None,
+        **kwargs,
     ):
-        super(POMO, self).__init__(env, policy, baseline)
-        self.policy = (
-            POMOPolicy(self.env, num_starts=num_starts, **policy_kwargs)
-            if policy is None
-            else policy
-        )
+        self.save_hyperparameters(logger=False)
 
-        # TODO: check baseline
-        self.baseline = SharedBaseline() if baseline is None else baseline
+        if policy is None:
+            policy = POMOPolicy(env.name, **policy_kwargs)
 
-        # POMO parameters
+        assert baseline == "shared", "POMO only supports shared baseline"
+
+        # Initialize with the shared baseline
+        super(POMO, self).__init__(env, policy, baseline, **kwargs)
+
+        self.num_starts = num_starts
         self.num_augment = num_augment
-        self.augment = (
-            StateAugmentation(self.env.name, num_augment) if num_augment > 1 else None
+        self.augment = StateAugmentation(
+            self.env.name, num_augment=self.num_augment, use_dihedral_8=use_dihedral_8
         )
 
-    def forward(self, td: TensorDict, phase: str = "train", extra=None, **policy_kwargs):
-        """Evaluate model, get costs and log probabilities and compare with baseline"""
+        # Add `_multistart` to decode type for train, val and test in policy
+        for phase in ["train", "val", "test"]:
+            self.set_decode_type_multistart(phase)
 
-        # Get num_starts from policy. If single_traj, set num_starts and num_augment to 0
-        num_starts = getattr(self.policy.decoder, "num_starts", 0)
-        num_augment = self.num_augment
-        if policy_kwargs.get("single_traj", False):
-            num_starts, num_augment = 0, 0
+    def shared_step(self, batch: Any, batch_idx: int, phase: str):
+        td = self.env.reset(batch)
+        n_aug, n_start = self.num_augment, self.num_starts
+        n_start = get_num_starts(td) if n_start is None else n_start
 
-        # during training, we do not augment the data
+        # During training, we do not augment the data
         if phase == "train":
-            num_augment = 0
-        elif num_augment > 1:
+            n_aug = 0
+        elif n_aug > 1:
             td = self.augment(td)
 
-        # Evaluate model, get costs and log probabilities
-        out = self.policy(td, phase, **policy_kwargs)
+        # Evaluate policy
+        out = self.policy(td, self.env, phase=phase, num_starts=n_start)
 
         # Unbatchify reward to [batch_size, num_augment, num_starts].
-        reward = unbatchify(out["reward"], (num_starts, num_augment))
+        reward = unbatchify(out["reward"], (n_start, n_aug))
 
         # Training phase
         if phase == "train":
-            assert num_starts > 1, "num_starts must be > 1 during training"
+            assert n_start > 1, "num_starts must be > 1 during training"
+            log_likelihood = unbatchify(out["log_likelihood"], (n_start, n_aug))
+            self.calculate_loss(td, batch, out, reward, log_likelihood)
 
-            ll = unbatchify(out["log_likelihood"], num_starts)
+        # Get multi-start (=POMO) rewards and best actions only during validation and test
+        else:
+            if n_start > 1:
+                # max multi-start reward
+                max_reward, max_idxs = reward.max(dim=1)
+                out.update({"max_reward": max_reward})
 
-            # REINFORCE loss: we consider the rewards instead of costs to be consistent with the literature
-            bl_val, bl_neg_loss = (
-                self.baseline.eval(td, reward)  # unbatched reward
-                if extra is None
-                else (extra, 0)
-            )
+                # Reshape batch to [batch, n_start, n_aug]
+                if out.get("actions", None) is not None:
+                    actions = unbatchify(out["actions"], (n_start, n_aug))
+                    out.update(
+                        {"best_multistart_actions": gather_by_index(actions, max_idxs)}
+                    )
+                    out["actions"] = actions
 
-            advantage = reward - bl_val  # advantage = reward - baseline
-            reinforce_loss = -(advantage * ll).mean()
-            loss = reinforce_loss - bl_neg_loss
-            out.update(
-                {
-                    "loss": loss,
-                    "reinforce_loss": reinforce_loss,
-                    "bl_loss": -bl_neg_loss,
-                    "bl_val": bl_val,
-                }
-            )
+            # Get augmentation score only during inference
+            if n_aug > 1:
+                # If multistart is enabled, we use the best multistart rewards
+                reward_ = max_reward if n_start > 1 else reward
+                max_aug_reward, max_idxs = reward_.max(dim=1)
+                out.update({"max_aug_reward": max_aug_reward})
+                if out.get("best_multistart_actions", None) is not None:
+                    out.update(
+                        {
+                            "best_aug_actions": gather_by_index(
+                                out["best_multistart_actions"], max_idxs
+                            )
+                        }
+                    )
 
-        # Get best actions and rewards
-        # Get POMO rewards and best actions
-        if num_starts > 1:
-            # Max POMO reward. Decouple augmentation and POMO
-            max_reward, max_idxs = reward.max(dim=1)
-            out.update({"max_reward": max_reward})
-
-            # Reshape batch to [batch, num_starts, num_augment]
-            if out.get("actions", None) is not None:
-                actions = unbatchify(out["actions"], (num_starts, num_augment))
-                out.update(
-                    {"best_multistart_actions": gather_by_index(actions, max_idxs)}
-                )
-                out["actions"] = actions
-
-        # Get augmentation score only during inference
-        if num_augment > 1:
-            if num_starts > 1:
-                # If POMO is enabled, we use the best POMO rewards
-                reward_ = max_reward
-            # [batch, num_augment]
-            max_aug_reward, max_idxs = reward_.max(dim=1)
-            out.update({"max_aug_reward": max_aug_reward})
-            if out.get("best_multistart_actions", None) is not None:
-                out.update(
-                    {
-                        "best_aug_actions": gather_by_index(
-                            out["best_multistart_actions"], max_idxs
-                        )
-                    }
-                )
-
-        return out
+        metrics = self.log_metrics(out, phase)
+        return {"loss": out.get("loss", None), **metrics}
--- rl4co/models/zoo/pomo/policy.py
+++ rl4co/models/zoo/pomo/policy.py
@@ -1,95 +1,45 @@
-import torch.nn as nn
-
-from tensordict.tensordict import TensorDict
-from torchrl.envs import EnvBase
-
-from rl4co.models.nn.graph.attnnet import GraphAttentionEncoder
-from rl4co.models.nn.utils import get_log_likelihood
-from rl4co.models.zoo.pomo.decoder import Decoder
-from rl4co.utils.pylogger import get_pylogger
-
-log = get_pylogger(__name__)
+from rl4co.models.zoo.common.autoregressive import AutoregressivePolicy
 
 
-class POMOPolicy(nn.Module):
+class POMOPolicy(AutoregressivePolicy):
+    """POMO model policy based on Kwon et al. (2020) http://arxiv.org/abs/2010.16011.
+    We re-declare the most important arguments here for convenience as in the paper.
+    See :class:`AutoregressivePolicy` superclass for more details.
+
+    Note:
+        Although the policy is the base :class:`AutoregressivePolicy`, we use the default values used in the paper.
+        Differently to the base class:
+        - `num_encoder_layers=6` (instead of 3)
+        - `normalization="instance"` (instead of "batch")
+        - `use_graph_context=False` (instead of True)
+        The latter is due to the fact that the paper does not use the graph context in the policy, which seems to be
+        helpful in overfitting to the training graph size.
+
+    Args:
+        env_name: Name of the environment used to initialize embeddings
+        embedding_dim: Dimension of the node embeddings
+        num_encoder_layers: Number of layers in the encoder
+        num_heads: Number of heads in the attention layers
+        normalization: Normalization type in the attention layers
+        **kwargs: keyword arguments passed to the :class:`AutoregressivePolicy`
+    """
+
     def __init__(
         self,
-        env: EnvBase,
-        encoder: nn.Module = None,
-        decoder: nn.Module = None,
+        env_name: str,
         embedding_dim: int = 128,
-        num_starts: int = 10,
         num_encoder_layers: int = 6,
-        normalization: str = "instance",
         num_heads: int = 8,
-        mask_inner: bool = True,
-        use_native_sdpa: bool = False,
-        force_flash_attn: bool = False,
-        train_decode_type: str = "sampling",
-        val_decode_type: str = "greedy",
-        test_decode_type: str = "greedy",
-        **unused_kw,
+        normalization: str = "instance",
+        use_graph_context: bool = False,
+        **kwargs,
     ):
-        super(POMOPolicy, self).__init__()
-        if len(unused_kw) > 0:
-            log.warn(f"Unused kwargs: {unused_kw}")
-
-        self.env = env
-
-        self.encoder = (
-            GraphAttentionEncoder(
-                num_heads=num_heads,
-                embedding_dim=embedding_dim,
-                num_layers=num_encoder_layers,
-                env=self.env,
-                normalization=normalization,
-                use_native_sdpa=use_native_sdpa,
-                force_flash_attn=force_flash_attn,
-            )
-            if encoder is None
-            else encoder
+        super(POMOPolicy, self).__init__(
+            env_name=env_name,
+            embedding_dim=embedding_dim,
+            num_encoder_layers=num_encoder_layers,
+            num_heads=num_heads,
+            normalization=normalization,
+            use_graph_context=use_graph_context,
+            **kwargs,
         )
-
-        self.decoder = (
-            Decoder(
-                self.env,
-                embedding_dim,
-                num_heads,
-                num_starts=num_starts,
-                mask_inner=mask_inner,
-                force_flash_attn=force_flash_attn,
-            )
-            if decoder is None
-            else decoder
-        )
-        self.train_decode_type = train_decode_type
-        self.val_decode_type = val_decode_type
-        self.test_decode_type = test_decode_type
-
-    def forward(
-        self,
-        td: TensorDict,
-        phase: str = "train",
-        return_actions: bool = False,
-        **decoder_kwargs,
-    ) -> dict:
-        # Encode inputs
-        embeddings, _ = self.encoder(td)
-
-        # Get decode type depending on phase
-        if decoder_kwargs.get("decode_type", None) is None:
-            decoder_kwargs["decode_type"] = getattr(self, f"{phase}_decode_type")
-
-        # Main rollout: autoregressive decoding
-        log_p, actions, td_out = self.decoder(td, embeddings, **decoder_kwargs)
-
-        # Log likelyhood is calculated within the model since returning it per action does not work well with
-        ll = get_log_likelihood(log_p, actions, td_out.get("mask", None))
-        out = {
-            "reward": td_out["reward"],
-            "log_likelihood": ll,
-        }
-        if return_actions:
-            out["actions"] = actions
-
-        return out
