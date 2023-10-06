1c25010dda9f56e6ba1f7901436554a642c25b7e
Federico Berto <berto.federico2@gmail.com>
2023-10-06 14:31:27 +0900

    [Feat] dataloader idx for POMO

--- rl4co/models/zoo/pomo/model.py
+++ rl4co/models/zoo/pomo/model.py
@@ -56,7 +56,7 @@ class POMO(REINFORCE):
         for phase in ["train", "val", "test"]:
             self.set_decode_type_multistart(phase)
 
-    def shared_step(self, batch: Any, batch_idx: int, phase: str):
+    def shared_step(self, batch: Any, batch_idx: int, phase: str, dataloader_idx: int = None):
         td = self.env.reset(batch)
         n_aug, n_start = self.num_augment, self.num_starts
         n_start = get_num_starts(td) if n_start is None else n_start
@@ -109,5 +109,5 @@ class POMO(REINFORCE):
                         }
                     )
 
-        metrics = self.log_metrics(out, phase)
+        metrics = self.log_metrics(out, phase, dataloader_idx=dataloader_idx)
         return {"loss": out.get("loss", None), **metrics}
