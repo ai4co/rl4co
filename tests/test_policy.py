d4acda9a044ae7ee6b547861e3535b43ef03bdc4
Federico Berto <berto.federico2@gmail.com>
2023-11-29 21:11:20 +0900

    [Feat] test POMO's MS with more envs #102

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -17,14 +17,17 @@ def test_base_policy(env_name, size=20, batch_size=2):
     assert out["reward"].shape == (batch_size,)
 
 
-@pytest.mark.parametrize("env_name", ["tsp", "cvrp", "pctsp", "spctsp"])
+@pytest.mark.parametrize(
+    "env_name", ["tsp", "cvrp", "pctsp", "spctsp", "sdvrp", "op", "pdp"]
+)
 def test_base_policy_multistart(env_name, size=20, batch_size=2):
     env, x = generate_env_data(env_name, size, batch_size)
     td = env.reset(x)
     policy = AutoregressivePolicy(env.name)
-    out = policy(td, env, decode_type="greedy_multistart", num_starts=size)
+    num_starts = size // 2 if env.name in ["pdp"] else size
+    out = policy(td, env, decode_type="greedy_multistart", num_starts=num_starts)
     assert out["reward"].shape == (
-        batch_size * size,
+        batch_size * num_starts,
     )  # to evaluate, we could just unbatchify
 
 
