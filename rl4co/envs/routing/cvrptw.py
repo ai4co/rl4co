62d1656ccc4c788e658563d6971f95019de2808b
ngastzepeda <n.gastzepeda@outlook.com>
2023-12-09 08:35:29 +0900

    extended time window logic

--- rl4co/envs/routing/cvrptw.py
+++ rl4co/envs/routing/cvrptw.py
@@ -1,3 +1,4 @@
+from math import sqrt
 from typing import Optional
 import torch
 from tensordict.tensordict import TensorDict
@@ -23,8 +24,8 @@ class CVRPTWEnv(CVRPEnv):
 
     def __init__(
         self,
-        min_time: float = 0.0,
-        max_time: float = 100.0,
+        min_time: int = 0,
+        max_time: int = 480,
         **kwargs,
     ):
         self.min_time = min_time
@@ -40,7 +41,7 @@ class CVRPTWEnv(CVRPEnv):
             low=self.min_time,
             high=self.max_time,
             shape=(self.num_loc, 1),
-            dtype=torch.float32,
+            dtype=torch.int64,
         )
 
         time_windows = BoundedTensorSpec(
@@ -50,7 +51,7 @@ class CVRPTWEnv(CVRPEnv):
                 self.num_loc,
                 2,
             ),  # each location has a 2D time window (start, end)
-            dtype=torch.float32,
+            dtype=torch.int64,
         )
 
         num_vehicles = UnboundedDiscreteTensorSpec(shape=(1), dtype=torch.int64)
@@ -68,26 +69,49 @@ class CVRPTWEnv(CVRPEnv):
 
     def generate_data(self, batch_size) -> TensorDict:
         td = super().generate_data(batch_size)
-        # batch_size = [td["locs"].shape[0]]
+
         batch_size = [batch_size] if isinstance(batch_size, int) else batch_size
 
         # initialize at zero
         current_time = torch.zeros(*batch_size, 1, dtype=torch.float32)
         num_vehicles = torch.ones(*batch_size, dtype=torch.int64)
 
-        # define time windows
-        min_ts = torch.FloatTensor(*batch_size, self.num_loc + 1).uniform_(
-            self.min_time, self.max_time
-        )
-        max_ts = torch.FloatTensor(*batch_size, self.num_loc + 1).uniform_(
-            self.min_time, self.max_time
-        )
-        min_times, max_times = torch.min(min_ts, max_ts), torch.max(min_ts, max_ts)
-        time_windows = torch.stack((min_times, max_times), dim=-1)
-        time_windows[..., 0, :] = 0.0  # depot has no time window
-
-        # first assume service durations of 0 (to be changed later)
+        ## define service durations
+        # generate randomly (first assume service durations of 0, to be changed later)
         durations = torch.zeros(*batch_size, self.num_loc + 1, dtype=torch.float32)
+
+        ## define time windows
+        # 1. get distances from depot
+        dist = get_distance(td["depot"], td["locs"].transpose(0, 1)).transpose(0, 1)
+        dist = torch.cat((torch.zeros(*batch_size, 1), dist), dim=1)
+        # 2. randomly create min and max times (as int) for all nodes incl. depot
+        min_ts = torch.randint(
+            self.min_time, self.max_time, (*batch_size, self.num_loc + 1)
+        )
+        max_ts = torch.randint(
+            self.min_time, self.max_time, (*batch_size, self.num_loc + 1)
+        )
+        # 3. set the lower value to min, the higher to max
+        min_times, max_times = torch.min(min_ts, max_ts), torch.max(min_ts, max_ts)
+        # 4. limit min and max times s.t. the distance to the depot is considered
+        max_times = torch.min(
+            self.max_time - dist, torch.max(max_times, self.min_time + dist + durations)
+        )
+        min_times = torch.max(
+            torch.min(min_times, max_times - durations), self.min_time + dist
+        )
+        assert torch.all(
+            min_times <= max_times
+        ), "Please make sure the relation between max_loc and max_time allows for feasible solutions."
+
+        # 5. stack to tensor time_windows
+        time_windows = torch.stack((min_times, max_times), dim=-1)
+        # 6. Adjust durations
+        durations = torch.min(durations, max_times - min_times)
+        # 7. the time window for the depot is set to [0,480]
+        time_windows[..., 0, 0] = 0  # depot has no time window
+        time_windows[..., 0, 1] = 480  # depot has no time window
+
         # for the case later that durations != 0 are used, the durations for the depot must still be 0
         durations[:, 0] = 0.0
         td.update(
@@ -102,27 +126,23 @@ class CVRPTWEnv(CVRPEnv):
 
     @staticmethod
     def get_action_mask(td: TensorDict) -> torch.Tensor:
-        masked = CVRPEnv.get_action_mask(td)
+        not_masked = CVRPEnv.get_action_mask(td)
         current_loc = gather_by_index(td["locs"], td["current_node"])
-        # mask_locs = torch.arange(td["locs"].size(1))[None, :] != td["current_node"]
-        # remaining_locs = (
-        #     td["locs"]
-        #     .masked_select(mask_locs.unsqueeze(-1))
-        #     .reshape([3, td["locs"].size(1) - 1, 2])
-        # )
         dist = get_distance(current_loc, td["locs"].transpose(0, 1)).transpose(0, 1)
+        td.update({"current_loc": current_loc, "distances": dist})
         can_reach_in_time = (
-            td["current_time"] + td["durations"] + dist
-            # + dist[mask_locs].reshape(td["locs"].size(0), td["locs"].size(1) - 1)
-            <= td["time_windows"][..., 1]
+            td["current_time"] + td["durations"] + dist <= td["time_windows"][..., 1]
         )
-        # TODO include logic to send vehicles back to the depot
-        # when their capacity is full but other customers still need to be served
-        # in which case we need to mask all nodes except the depot and reset current_time to 0
-        return ~masked & can_reach_in_time
+        return not_masked & can_reach_in_time
 
-    # def _step(self, td: TensorDict) -> TensorDict:
-    #     return super()._step(td)
+    def _step(self, td: TensorDict) -> TensorDict:
+        # update current_time
+        batch_size = td["locs"].shape[0]
+        distance = gather_by_index(td["distances"], td["action"]).reshape([batch_size, 1])
+        duration = gather_by_index(td["durations"], td["action"]).reshape([batch_size, 1])
+        td["current_time"] = td["current_time"] + distance + duration
+        td = super()._step(td)
+        return td
 
     def _reset(
         self, td: Optional[TensorDict] = None, batch_size: Optional[list] = None
