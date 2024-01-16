c3a6b16c8208435fbc915b62f8fd7ce074a9f58f
ngastzepeda <n.gastzepeda@outlook.com>
2024-01-17 02:55:17 +0900

    added functions load_solomon_instance() and load_solomon_solution() in rl4co/data/utils.py, ntbc

--- rl4co/data/utils.py
+++ rl4co/data/utils.py
@@ -1,8 +1,12 @@
 import os
 
 import numpy as np
+import vrplib
 
 from tensordict.tensordict import TensorDict
+
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_PATH = os.path.dirname(os.path.dirname(CURR_DIR))
 
 
 def load_npz_to_tensordict(filename):
@@ -21,3 +25,22 @@ def check_extension(filename, extension=".npz"):
     if os.path.splitext(filename)[1] != extension:
         return filename + extension
     return filename
+
+
+def load_solomon_instance(name, path=None):
+    """Load a solomon instance from a file"""
+    if not path:
+        path = "data/solomon/instances/"
+        path = os.path.join(ROOT_PATH, path)
+    if not os.path.isfile(f"{path}{name}.txt"):
+        vrplib.download_instance(name=name, path=path)
+    return vrplib.read_instance(path=f"{path}{name}.txt", instance_format="solomon")
+
+
+def load_solomon_solution(name, path=None):
+    if not path:
+        path = "data/solomon/solutions/"
+        path = os.path.join(ROOT_PATH, path)
+    if not os.path.isfile(f"{path}{name}.sol"):
+        vrplib.download_solution(name=name, path=path)
+    return vrplib.read_solution(path=f"{path}{name}.sol")
