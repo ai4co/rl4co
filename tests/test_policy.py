b6225fe4982059ed43de4dbbb875817120a854a4
Laurin Luttmann <laurin.luttmann@gmail.com>
2024-01-23 17:31:14 +0100

    add test

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -31,6 +31,27 @@ def test_base_policy_multistart(env_name, size=20, batch_size=2):
     )  # to evaluate, we could just unbatchify
 
 
+@pytest.mark.parametrize(
+    "env_name",
+    ["tsp", "cvrp", "pctsp", "spctsp", "sdvrp", "op", "pdp"],
+)
+@pytest.mark.parametrize("select_best", [True, False])
+def test_base_policy_beam_search(env_name, select_best, size=20, batch_size=2):
+    env, x = generate_env_data(env_name, size, batch_size)
+    td = env.reset(x)
+    policy = AutoregressivePolicy(env.name)
+    beam_width = size // 2 if env.name in ["pdp"] else size
+    out = policy(
+        td, env, decode_type="beam_search", beam_width=beam_width, select_best=select_best
+    )
+
+    if select_best:
+        expected_shape = (batch_size,)
+    else:
+        expected_shape = (batch_size * beam_width,)
+    assert out["reward"].shape == expected_shape
+
+
 def test_pointer_network(size=20, batch_size=2):
     env, x = generate_env_data("tsp", size, batch_size)
     td = env.reset(x)
