5827d912bc05d68e64fa7a82081328a17b47dcf7
ngastzepeda <nayeli.gast@uni-bielefeld.de>
2024-02-27 11:28:31 +0100

    manual euclidean distance calculation (torch.norm rounds to 4 decimals)

--- rl4co/utils/ops.py
+++ rl4co/utils/ops.py
@@ -78,7 +78,13 @@ def gather_by_index(src, idx, dim=1, squeeze=True):
 @torch.jit.script
 def get_distance(x: Tensor, y: Tensor):
     """Euclidean distance between two tensors of shape `[..., n, dim]`"""
-    return (x - y).norm(p=2, dim=-1)
+    squared_diff = (x - y) ** 2  # Compute element-wise squared differences
+    summed_squared_diff = torch.sum(squared_diff, dim=-1)  # Sum along the last dimension
+    distance = torch.sqrt(
+        summed_squared_diff
+    )  # Take square root to get the Euclidean distance
+    return distance
+    # return (x - y).norm(p=2, dim=-1)
 
 
 @torch.jit.script
