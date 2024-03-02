627ed4784ffd1183f776ff5b38a68ae6ac924280
Federico Berto <berto.federico2@gmail.com>
2024-03-02 18:08:49 +0900

    [Test] pass `select_start_nodes_fn` too

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -2,6 +2,7 @@ import pytest
 
 from rl4co.models import AutoregressivePolicy, PointerNetworkPolicy
 from rl4co.utils.test_utils import generate_env_data
+from rl4co.utils.ops import select_start_nodes
 
 
 # Main autorergressive policy: rollout over multiple envs since it is the base
@@ -25,7 +26,13 @@ def test_base_policy_multistart(env_name, size=20, batch_size=2):
     td = env.reset(x)
     policy = AutoregressivePolicy(env.name)
     num_starts = size // 2 if env.name in ["pdp"] else size
-    out = policy(td, env, decode_type="multistart_greedy", num_starts=num_starts)
+    out = policy(
+        td,
+        env,
+        decode_type="multistart_greedy",
+        num_starts=num_starts,
+        select_start_nodes_fn=select_start_nodes,
+    )
     assert out["reward"].shape == (
         batch_size * num_starts,
     )  # to evaluate, we could just unbatchify
