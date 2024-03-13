7b942f52595c3953923c5caabe5a3e14bf88ae34
Laurin Luttmann <laurin.luttmann@gmail.com>
2024-03-13 13:27:12 +0100

    modularized ffsp script

--- rl4co/envs/scheduling/ffsp.py
+++ rl4co/envs/scheduling/ffsp.py
@@ -141,7 +141,6 @@ class FFSPEnv(RL4COEnvBase):
         self.max_time = max_time
         self.flatten_stages = flatten_stages
         self.tables = IndexTables(num_stage, num_machine, flatten_stages, self.device)
-        self.cnts = []
 
     # TODO make envs implement get_num_starts and select_start_nodes functions
     # def get_num_starts(self, td):
@@ -152,6 +151,131 @@ class FFSPEnv(RL4COEnvBase):
     #     selected = torch.full((num_starts * td.size(0),), self.num_job)
     #     return selected
 
+    def _move_to_next_machine(self, td):
+        batch_size = td.batch_size
+        batch_idx = torch.arange(*batch_size, dtype=torch.long, device=td.device)
+
+        time_idx = td["time_idx"]
+        machine_idx = td["machine_idx"]
+        sub_time_idx = td["sub_time_idx"]
+
+        machine_wait_step = td["machine_wait_step"]
+        job_wait_step = td["job_wait_step"]
+        job_location = td["job_location"]
+
+        ready = torch.flatten(td["done"])
+        idx = torch.flatten(batch_idx)
+        # select minibatch instances that need updates (are not done)
+        idx = idx[~ready]
+
+        while ~ready.all():
+            # increment the stage-machine counter
+            new_sub_time_idx = sub_time_idx[idx] + 1
+            # increment time if all machines-stage combinations have been candidates
+            step_time_required = new_sub_time_idx == self.num_machine_total
+            time_idx[idx] += step_time_required.long()
+            # in this case set the machine-stage counter to zero again
+            new_sub_time_idx[step_time_required] = 0
+            # update machine-stage counter
+            sub_time_idx[idx] = new_sub_time_idx
+            # determine current machine candidate
+            new_machine_idx = self.tables.get_machine_index(new_sub_time_idx, idx)
+            machine_idx[idx] = new_machine_idx
+
+            # decrease machine wait time by 1 if instance transitioned to new time step
+            machine_wait_steps = machine_wait_step[idx, :]
+            machine_wait_steps[step_time_required, :] -= 1
+            machine_wait_steps[machine_wait_steps < 0] = 0
+            machine_wait_step[idx, :] = machine_wait_steps
+
+            # decrease job wait time by 1 if instance transitioned to new time step
+            job_wait_steps = job_wait_step[idx, :]
+            job_wait_steps[step_time_required, :] -= 1
+            job_wait_steps[job_wait_steps < 0] = 0
+            job_wait_step[idx, :] = job_wait_steps
+            # machine is ready if its wait time is zero
+            machine_ready = machine_wait_step[idx, new_machine_idx] == 0
+            # job is ready if the current stage matches the stage of the job and
+            # its wait time is zero (no operation of previous stage is in process)
+            new_stage_idx = self.tables.get_stage_index(new_sub_time_idx)
+            job_ready_1 = job_location[idx, : self.num_job] == new_stage_idx[:, None]
+            job_ready_2 = job_wait_step[idx, : self.num_job] == 0
+            job_ready = (job_ready_1 & job_ready_2).any(dim=-1)
+            # instance ready if at least one job and the current machine are ready
+            ready = machine_ready & job_ready
+            idx = idx[~ready]
+
+        return td.update(
+            {
+                "time_idx": time_idx,
+                "sub_time_idx": sub_time_idx,
+                "machine_idx": machine_idx,
+                "machine_wait_step": machine_wait_step,
+                "job_wait_step": job_wait_step,
+            }
+        )
+
+    def _update_step_state(self, td):
+        batch_size = td.batch_size
+        batch_idx = torch.arange(*batch_size, dtype=torch.long, device=td.device)
+
+        sub_time_idx = td["sub_time_idx"]
+        job_location = td["job_location"]
+        job_wait_step = td["job_wait_step"]
+        done = td["done"]
+
+        # update stage
+        stage_idx = self.tables.get_stage_index(sub_time_idx)
+        stage_machine_idx = self.tables.get_stage_machine_index(sub_time_idx, batch_idx)
+
+        job_loc = job_location[:, : self.num_job]
+        job_wait_time = job_wait_step[:, : self.num_job]
+        # determine if job can be scheduled in current stage
+        # (i.e. previous stages are completed)
+        job_in_stage = job_loc == stage_idx[:, None]
+        job_not_waiting = job_wait_time == 0
+        # job can be scheduled if in current stage and not waiting
+        job_available = job_in_stage & job_not_waiting
+        # determine instance for which waiting is allowed. This is the case if either
+        # 1.) any of its jobs need to be scheduled in a previous stage,
+        # 2.) any of the jobs wait for an operation of the preceeding stage to finish
+        # 3.) the instance is done.
+        job_in_previous_stages = (job_loc < stage_idx[:, None]).any(dim=-1)
+        job_waiting_in_stage = (job_in_stage & (job_wait_time > 0)).any(dim=-1)
+        wait_allowed = job_in_previous_stages + job_waiting_in_stage + done
+
+        job_enable = torch.cat((job_available, wait_allowed[:, None]), dim=-1)
+        job_mask = torch.full(
+            size=(*batch_size, self.num_job + 1),
+            dtype=torch.bool,
+            device=self.device,
+            fill_value=0,
+        )
+        job_mask[job_enable] = 1
+
+        if self.flatten_stages:
+            cost_matrix = td["run_time"].flatten(-2, -1)
+        else:
+            cost_matrix = (
+                td["run_time"]
+                .gather(
+                    3,
+                    stage_idx[:, None, None, None].expand(
+                        *batch_size, self.num_job, self.num_machine, 1
+                    ),
+                )
+                .squeeze(-1)
+            )
+
+        return td.update(
+            {
+                "cost_matrix": cost_matrix,
+                "action_mask": job_mask,
+                "stage_idx": stage_idx,
+                "stage_machine_idx": stage_machine_idx,
+            }
+        )
+
     def _step(self, td: TensorDict) -> TensorDict:
         batch_size = td.batch_size
         batch_idx = torch.arange(*batch_size, dtype=torch.long, device=td.device)
@@ -160,7 +284,7 @@ class FFSPEnv(RL4COEnvBase):
         job_idx = td["action"]
         time_idx = td["time_idx"]
         machine_idx = td["machine_idx"]
-        sub_time_idx = td["sub_time_idx"]
+
         # insert start time of the selected job in the schedule
         schedule = td["schedule"]
         schedule[batch_idx, machine_idx, job_idx] = time_idx
@@ -178,16 +302,21 @@ class FFSPEnv(RL4COEnvBase):
         # determine whether all jobs are scheduled
         done = (job_location[:, : self.num_job] == self.num_stage).all(dim=-1)
 
-        update_dict = {
-            "time_idx": time_idx,  # (bs)
-            "sub_time_idx": sub_time_idx,  # (bs)
-            "machine_idx": machine_idx,  # (bs)
-            "schedule": schedule,  # # (bs, ops, jobs+1)
-            "machine_wait_step": machine_wait_step,  # (bs, ops)
-            "job_location": job_location,  # (bs, jobs+1)
-            "job_wait_step": job_wait_step,  # (bs, jobs+1)
-            "done": done,  # (bs)
-        }
+        td = td.update(
+            {
+                "schedule": schedule,
+                "machine_wait_step": machine_wait_step,
+                "job_location": job_location,
+                "job_wait_step": job_wait_step,
+                "done": done,
+            }
+        )
+
+        if done.all():
+            pass
+        else:
+            td = self._move_to_next_machine(td)
+            td = self._update_step_state(td)
 
         if done.all():
             # determine end times of ops by adding the durations to their start times
@@ -197,109 +326,8 @@ class FFSPEnv(RL4COEnvBase):
             # determine the max makespan of all jobs
             end_time_max, _ = end_time_max.max(dim=-1)
             reward = -end_time_max.to(torch.float32)
-            update_dict["reward"] = reward
+            td.set("reward", reward)
 
-        else:
-            ready = torch.flatten(done)
-            idx = torch.flatten(batch_idx.clone())
-            # select minibatch instances that need updates (are not done)
-            idx = idx[~ready]
-            cnt = 0
-            while ~ready.all():
-                # increment the stage-machine counter
-                new_sub_time_idx = sub_time_idx[idx] + 1
-                # increment time if all machines-stage combinations have been candidates
-                step_time_required = new_sub_time_idx == self.num_machine_total
-                time_idx[idx] += step_time_required.long()
-                # in this case set the machine-stage counter to zero again
-                new_sub_time_idx[step_time_required] = 0
-                # update machine-stage counter
-                sub_time_idx[idx] = new_sub_time_idx
-                # determine current machine candidate
-                new_machine_idx = self.tables.get_machine_index(new_sub_time_idx, idx)
-                machine_idx[idx] = new_machine_idx
-
-                # decrease machine wait time by 1 if instance transitioned to new time step
-                machine_wait_steps = machine_wait_step[idx, :]
-                machine_wait_steps[step_time_required, :] -= 1
-                machine_wait_steps[machine_wait_steps < 0] = 0
-                machine_wait_step[idx, :] = machine_wait_steps
-
-                # decrease job wait time by 1 if instance transitioned to new time step
-                job_wait_steps = job_wait_step[idx, :]
-                job_wait_steps[step_time_required, :] -= 1
-                job_wait_steps[job_wait_steps < 0] = 0
-                job_wait_step[idx, :] = job_wait_steps
-                # machine is ready if its wait time is zero
-                machine_ready = machine_wait_step[idx, new_machine_idx] == 0
-                # job is ready if the current stage matches the stage of the job and
-                # its wait time is zero (no operation of previous stage is in process)
-                new_stage_idx = self.tables.get_stage_index(new_sub_time_idx)
-                job_ready_1 = job_location[idx, : self.num_job] == new_stage_idx[:, None]
-                job_ready_2 = job_wait_step[idx, : self.num_job] == 0
-                job_ready = (job_ready_1 & job_ready_2).any(dim=-1)
-                # instance ready if at least one job and the current machine are ready
-                ready = machine_ready & job_ready
-                idx = idx[~ready]
-
-                cnt += 1
-            self.cnts.append(cnt)
-            # update stage
-            stage_idx = self.tables.get_stage_index(sub_time_idx)
-            stage_machine_idx = self.tables.get_stage_machine_index(
-                sub_time_idx, batch_idx
-            )
-
-            job_loc = job_location[:, : self.num_job]
-            job_wait_time = job_wait_step[:, : self.num_job]
-            # determine if job can be scheduled in current stage
-            # (i.e. previous stages are completed)
-            job_in_stage = job_loc == stage_idx[:, None]
-            job_not_waiting = job_wait_time == 0
-            # job can be scheduled if in current stage and not waiting
-            job_available = job_in_stage & job_not_waiting
-            # determine instance for which waiting is allowed. This is the case if either
-            # 1.) any of its jobs need to be scheduled in a previous stage,
-            # 2.) any of the jobs wait for an operation of the preceeding stage to finish
-            # 3.) the instance is done.
-            job_in_previous_stages = (job_loc < stage_idx[:, None]).any(dim=-1)
-            job_waiting_in_stage = (job_in_stage & (job_wait_time > 0)).any(dim=-1)
-            wait_allowed = job_in_previous_stages + job_waiting_in_stage + done
-
-            job_enable = torch.cat((job_available, wait_allowed[:, None]), dim=-1)
-            job_mask = torch.full(
-                size=(*batch_size, self.num_job + 1),
-                dtype=torch.bool,
-                device=self.device,
-                fill_value=0,
-            )
-            job_mask[job_enable] = 1
-
-            if self.flatten_stages:
-                cost_matrix = td["run_time"].flatten(-2, -1)
-            else:
-                cost_matrix = (
-                    td["run_time"]
-                    .gather(
-                        3,
-                        stage_idx[:, None, None, None].expand(
-                            *batch_size, self.num_job, self.num_machine, 1
-                        ),
-                    )
-                    .squeeze(-1)
-                )
-
-            update_dict.update(
-                {
-                    "cost_matrix": cost_matrix,
-                    "action_mask": job_mask,
-                    "stage_idx": stage_idx,
-                    "stage_machine_idx": stage_machine_idx,
-                }
-            )
-
-        # Updated state
-        td.update(update_dict)
         return td
 
     def _reset(
