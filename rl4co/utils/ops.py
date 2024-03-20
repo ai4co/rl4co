16318c65938230ecec875c914caa9bc752015c3b
Laurin Luttmann <laurin.luttmann@gmail.com>
2024-03-20 12:30:19 +0100

    dont flatten by default in FFSP

--- rl4co/utils/ops.py
+++ rl4co/utils/ops.py
@@ -113,7 +113,7 @@ def get_num_starts(td, env_name=None):
             num_starts - 1
         ) // 2  # only half of the nodes (i.e. pickup nodes) can be start nodes
     elif env_name in ["cvrp", "sdvrp", "mtsp", "op", "pctsp", "spctsp"]:
-        num_starts = num_starts - 1  # depot / dummy job cannot be a start node
+        num_starts = num_starts - 1  # depot cannot be a start node
 
     return num_starts
 
