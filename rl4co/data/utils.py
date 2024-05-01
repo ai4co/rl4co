f6486a1304e0e63e6b826e6c5c8387239099ae2c
Federico Berto <berto.federico2@gmail.com>
2024-05-01 17:12:20 +0900

    [Feat] save TensorDict to npz
    Co-authored-by: ngastzepeda <n.gastzepeda@outlook.com>

--- rl4co/data/utils.py
+++ rl4co/data/utils.py
@@ -19,6 +19,17 @@ def load_npz_to_tensordict(filename):
     return TensorDict(x_dict, batch_size=batch_size)
 
 
+def save_tensordict_to_npz(tensordict, filename, compress: bool = False):
+    """Save a TensorDict to a npz file
+    We assume that the TensorDict contains a dictionary of tensors
+    """
+    x_dict = {k: v.numpy() for k, v in tensordict.items()}
+    if compress:
+        np.savez_compressed(filename, **x_dict)
+    else:
+        np.savez(filename, **x_dict)
+
+
 def check_extension(filename, extension=".npz"):
     """Check that filename has extension, otherwise add it"""
     if os.path.splitext(filename)[1] != extension:
