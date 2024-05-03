ea12d85fc780855058f1679e28b0aa00290ba6cf
ahottung <andre.hottung@uni-bielefeld.de>
2024-05-03 16:07:44 +0200

    Updating init files

--- rl4co/models/zoo/__init__.py
+++ rl4co/models/zoo/__init__.py
@@ -10,6 +10,7 @@ from rl4co.models.zoo.ham import (
 )
 from rl4co.models.zoo.matnet import MatNet, MatNetPolicy
 from rl4co.models.zoo.mdam import MDAM, MDAMPolicy
+from rl4co.models.zoo.polynet import PolyNet
 from rl4co.models.zoo.pomo import POMO
 from rl4co.models.zoo.ppo import PPOModel
 from rl4co.models.zoo.ptrnet import PointerNetwork, PointerNetworkPolicy
