c546234360e4a5e772aa8a3ae8e58e85d0698492
Federico Berto <berto.federico2@gmail.com>
2024-05-05 22:10:42 +0900

    [BugFix] minor capacity bugfix

--- rl4co/envs/routing/cvrp/generator.py
+++ rl4co/envs/routing/cvrp/generator.py
@@ -96,21 +96,20 @@ class CVRPGenerator(Generator):
             )
 
         # Capacity
-        self.capacity = kwargs.get("capacity", None)
         if (
-            self.capacity is None
+            capacity is None
         ):  # If not provided, use the default capacity from Kool et al. 2019
-            self.capacity = CAPACITIES.get(num_loc, None)
+            capacity = CAPACITIES.get(num_loc, None)
         if (
-            self.capacity is None
+            capacity is None
         ):  # If not in the table keys, find the closest number of nodes as the key
             closest_num_loc = min(CAPACITIES.keys(), key=lambda x: abs(x - num_loc))
-            self.capacity = CAPACITIES[closest_num_loc]
+            capacity = CAPACITIES[closest_num_loc]
             log.warning(
-                f"The capacity capacity for {num_loc} locations is not defined. Using the closest capacity: {self.capacity}\
+                f"The capacity capacity for {num_loc} locations is not defined. Using the closest capacity: {capacity}\
                     with {closest_num_loc} locations."
             )
-        # TODO: when setting attribute num_loc, update the capacity
+        self.capacity = capacity
 
     def _generate(self, batch_size) -> TensorDict:
         # Sample locations
