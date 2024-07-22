b26a951b619a2fa7e62b55512c470c1c066894fd
Federico Berto <berto.federico2@gmail.com>
2024-07-23 00:23:02 +0900

    [Docs] remove Home via custom styling

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import shutil
+from textwrap import dedent
 
 log = logging.getLogger("mkdocs")
 
@@ -17,9 +19,34 @@ def on_startup(*args, **kwargs):
             created_files.append(init_file_path)
     log.info(f"{len(created_files)} __init__.py files created")
 
+    log.info("Copying README.md to docs/index.md and adding custom CSS")
+    shutil.copyfile("README.md", "docs/index.md")
+
+    def append_css_to_readme(file_path):
+        css_content = dedent("""
+        <style type="text/css">
+        .md-typeset h1,
+        .md-content__button {
+            display: none;
+        }
+        </style>
+        """)    
+        if not os.path.exists(file_path):
+            print(f"Error: The file {file_path} does not exist.")
+            return        
+        with open(file_path, 'a') as file:
+            file.write(css_content)
+        print(f"CSS content has been appended to {file_path}")
+
+    append_css_to_readme("docs/index.md")
+
 
 def on_shutdown(*args, **kwargs):
     log.info(f"Removing {len(created_files)} created __init__.py files")
     for file_path in created_files:
         if os.path.exists(file_path):
             os.remove(file_path)
+
+    log.info("Removing docs/index.md")
+    if os.path.exists("docs/index.md"):
+        os.remove("docs/index.md")
